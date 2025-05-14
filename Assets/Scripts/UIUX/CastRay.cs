41e7b6454e8a91ece800fc9ec636ea8bc84bf6af
ReubenBidoof <rubenhoenderken@gmail.com>
2025-05-14 20:44:41 +0200

    Added all the prefabs (i think)

---
+++ Assets/Scripts/UIUX/CastRay.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using TMPro;
+
+public class castRay : MonoBehaviour
+{
+    Ray ray;
+    RaycastHit hit;
+    public TextMeshProUGUI tooltipText;
+    private bool pressingE = false;
+    private Transform player;
+    public float objectRange = 1f;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        // Validate mouse position
+        if (Input.mousePosition.x < 0 || Input.mousePosition.y < 0 ||
+            Input.mousePosition.x > Screen.width || Input.mousePosition.y > Screen.height)
+        {
+            tooltipText.gameObject.SetActive(false);
+            return;
+        }
+
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.transform.CompareTag("NPC") || hit.transform.CompareTag("Flower"))
+            {
+                float distanceToHit = Vector3.Distance(hit.transform.position, player.position);
+
+                if (distanceToHit <= objectRange)
+                {
+                    tooltipText.text = "Press E";
+                    tooltipText.gameObject.SetActive(true);
+                }
+            }
+        }
+
+        tooltipText.gameObject.SetActive(false);
+    }
+
+    private void OnGUI()
+    {
+        if (pressingE)
+        {
+            GUIStyle e = new GUIStyle();
+            e.fontSize = 20;
+            GUI.Label(new Rect(630, 20, 300, 100), "Text here :D", e);
+        }
+    }
+}

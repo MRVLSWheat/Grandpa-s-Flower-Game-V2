93c3673bda04c6641b0d48645ef8bc5659015dc8
ReubenBidoof <rubenhoenderken@gmail.com>
2025-05-21 12:28:52 +0200

    adding lights :D

---
+++ Assets/Editor/AutomaticLightingEditor.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(AutomaticLighting))]
+public class AutomaticLightingEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        // Draw default inspector
+        DrawDefaultInspector();
+
+        AutomaticLighting lighting = (AutomaticLighting)target;
+
+        GUILayout.Space(10);
+
+        // Generate Button
+        if (GUILayout.Button("Generate Light Probes"))
+        {
+            lighting.Generate();
+        }
+
+        // Bake Button
+        if (GUILayout.Button("Bake Light Probes"))
+        {
+            // This triggers Unity's light probe baking
+            Lightmapping.BakeAsync();
+        }
+    }
+}
